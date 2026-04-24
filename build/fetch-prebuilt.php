08ca98d2aa309c15bca9aaa95d3b8cdf73672d86
jayeshmepani <jayesh@shreesoftech.com>
2026-04-24 15:41:28 +0530

    chore: bump version to 1.0.1 and ensure all version strings are synced

--- build/fetch-prebuilt.php
+++ build/fetch-prebuilt.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-$postalkitVersion = "v1.0.0";
+$postalkitVersion = "v1.0.1";
 $repoUrl = "https://github.com/jayeshmepani/libpostal-ffi-python/releases/download/{$postalkitVersion}";
 
 $targets = [
